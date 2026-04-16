a2a879d4281a79194f1c0c711e82a04351f47360
2014000044IlanMotta <i.motta.5bdb@gmail.com>
2026-04-15 21:42:08 -0600

    Add files via upload

---
+++ ListaDeValores/src/main/java/com/mycompany/listadevalores/ListaDeValores.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.listadevalores;
+
+/**
+ *
+ * @author carlo
+ */
+public class ListaDeValores {
+
+    public static void main(String[] args) {
+        new NewJFrame().setVisible(true);
+        
+    }
+}
